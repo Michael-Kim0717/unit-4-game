889dfa24f4712cccd25b708bfa57ff65d94bc03a
Michael Kim <MichaelKim0717@gmail.com>
2018-06-09 12:55:02 -0400

    Health bars and alignment added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,6 +18,22 @@ $(document).ready(function(){
     /* Variable to determine winner */
     var defenderCounter = 0;
 
+    /* Variables for HP */
+    /* var hBar = $('.health-bar'),
+        bar = hBar.find('.bar'),
+        hit = hBar.find('.hit'); */
+    var 
+        attackerBar = $("#attackerHP");
+        defenderBar = $("#defenderHP");
+        a_Total_Bar = attackerBar.find("#a-total-bar");
+        a_Hit_Bar = attackerBar.find("#a-hit-bar");
+        d_Total_Bar = defenderBar.find("#d-total-bar");
+        d_Hit_Bar = defenderBar.find("#d-hit-bar");
+
+    // When the first card is selected,
+    // If there is no attacker, Bulbasaur will become our attacker,
+    // Else, if there is no defender, Bulbasaur will become our defender.
+    // Also sets images where applicable.
     $("#c1").on("click", function(){
         if (attacker.attack == 0){
             attacker = {
@@ -26,6 +42,8 @@ $(document).ready(function(){
                 attack: 22,
                 bAttack: 22
             }
+            attackerBar.data('total', 75);
+            attackerBar.data('value', 75);
             $("#c1").css("background-color", "#FBA5A5");
             $("#attackerArea").attr("src", "./assets/images/bulbasaur.png");
             $("#attackerArea").addClass("flipped");
@@ -37,6 +55,12 @@ $(document).ready(function(){
                 attack: 22,
                 cAttack: 3
             }
+            defenderBar.data('total', 75);
+            defenderBar.data('value', 75);
+                
+            d_Hit_Bar.css({'width': '0'});
+            d_Total_Bar.css('width', '100%');
+            
             $("#c1").css("background-color", "#FBA5A5");
             $("#defenderArea").attr("src", "./assets/images/bulbasaur.png");
         }
@@ -44,6 +68,10 @@ $(document).ready(function(){
         console.log(defender.name + ", " + defender.health + ", " + defender.attack + ", " + defender.cAttack);
     });
 
+    // When the second card is selected,
+    // If there is no attacker, Charmander will become our attacker,
+    // Else, if there is no defender, Charmander will become our defender.
+    // Also sets images where applicable.
     $("#c2").on("click", function(){
         if (attacker.attack == 0){
             attacker = {
@@ -52,6 +80,8 @@ $(document).ready(function(){
                 attack: 27,
                 bAttack: 27
             }
+            attackerBar.data('total', 50);
+            attackerBar.data('value', 50);
             $("#c2").css("background-color", "#FBA5A5");
             $("#attackerArea").attr("src", "./assets/images/charmander.png");
             $("#attackerArea").addClass("flipped");
@@ -63,6 +93,12 @@ $(document).ready(function(){
                 attack: 27,
                 cAttack: 6
             }
+            defenderBar.data('total', 50);
+            defenderBar.data('value', 50);
+
+            d_Hit_Bar.css({'width': '0'});
+            d_Total_Bar.css('width', '100%');
+
             $("#c2").css("background-color", "#FBA5A5");
             $("#defenderArea").attr("src", "./assets/images/charmander.png");
         }
@@ -70,6 +106,10 @@ $(document).ready(function(){
         console.log(defender.name + ", " + defender.health + ", " + defender.attack + ", " + defender.cAttack);
     });
 
+    // When the third card is selected,
+    // If there is no attacker, Squirtle will become our attacker,
+    // Else, if there is no defender, Squirtle will become our defender.
+    // Also sets images where applicable.
     $("#c3").on("click", function(){
         if (attacker.attack == 0){
             attacker = {
@@ -78,6 +118,8 @@ $(document).ready(function(){
                 attack: 18,
                 bAttack: 18
             }
+            attackerBar.data('total', 100);
+            attackerBar.data('value', 100);
             $("#c3").css("background-color", "#FBA5A5");
             $("#attackerArea").attr("src", "./assets/images/squirtle.png");
             $("#attackerArea").addClass("flipped");
@@ -89,6 +131,12 @@ $(document).ready(function(){
                 attack: 18,
                 cAttack: 2
             }
+            defenderBar.data('total', 100);
+            defenderBar.data('value', 100);
+            
+            d_Hit_Bar.css({'width': '0'});
+            d_Total_Bar.css('width', '100%');
+
             $("#c3").css("background-color", "#FBA5A5");
             $("#defenderArea").attr("src", "./assets/images/squirtle.png");
         } 
@@ -96,6 +144,10 @@ $(document).ready(function(){
         console.log(defender.name + ", " + defender.health + ", " + defender.attack + ", " + defender.cAttack);
     });
 
+    // When the last card is selected,
+    // If there is no attacker, Pikachu will become our attacker,
+    // Else, if there is no defender, Pikachu will become our defender.
+    // Also sets images where applicable.
     $("#c4").on("click", function(){
         if (attacker.attack == 0){
             attacker = {
@@ -104,6 +156,8 @@ $(document).ready(function(){
                 attack: 31,
                 bAttack: 31
             }
+            attackerBar.data('total', 44);
+            attackerBar.data('value', 44);
             $("#c4").css("background-color", "#FBA5A5");
             $("#attackerArea").attr("src", "./assets/images/pikachu.png");
             $("#attackerArea").addClass("flipped");
@@ -115,6 +169,12 @@ $(document).ready(function(){
                 attack: 31,
                 cAttack: 5
             }
+            defenderBar.data('total', 44);
+            defenderBar.data('value', 44);
+
+            d_Hit_Bar.css({'width': '0'});
+            d_Total_Bar.css('width', '100%');
+            
             $("#c4").css("background-color", "#FBA5A5");
             $("#defenderArea").attr("src", "./assets/images/pikachu.png");
         }
@@ -122,8 +182,52 @@ $(document).ready(function(){
         console.log(defender.name + ", " + defender.health + ", " + defender.attack + ", " + defender.cAttack);
     });
 
+    // When the 'Attack' button is pressed,
+    // * Create an error if there is no attacker or defender *
+    // Damage the defender by the attacker's attack value and increase the attacker's attack value by the base value.
+    // Damage the attacker by the defender's counter attack value.
+
     $("#attack").on("click", function(){
+
+        var 
+            aTotal = attackerBar.data('total'),
+            aValue = attackerBar.data('value'),
+
+            dTotal = defenderBar.data('total'),
+            dValue = defenderBar.data('value');
+
+        // Change attacker's and defender's health bar.
         if (attacker.attack != 0 && defender.attack != 0){
+            var dDamage = defender.cAttack;
+            var aHP = aValue - dDamage;
+            var aBarWidth = (aHP / aTotal) * 100;
+            var aHitWidth = (dDamage / aValue) * 100 + "%";
+            if ((dDamage / aValue) > aBarWidth){
+                aHitWidth = aBarWidth + "%";
+            }
+
+            var aDamage = attacker.attack;
+            var dHP = dValue - aDamage;
+            var dBarWidth = (dHP / dTotal) * 100;
+            var dHitWidth = (aDamage / dValue) * 100 + "%";
+            if ((aDamage / dValue) > dBarWidth){
+                dHitWidth = dBarWidth + "%";
+            }
+
+            a_Hit_Bar.css('width', aHitWidth);
+            attackerBar.data('value', aHP);
+
+            d_Hit_Bar.css('width', dHitWidth);
+            defenderBar.data('value', dHP);
+
+            setTimeout(function(){
+                a_Hit_Bar.css({'width': '0'});
+                a_Total_Bar.css('width', aBarWidth + "%");
+                
+                d_Hit_Bar.css({'width': '0'});
+                d_Total_Bar.css('width', dBarWidth + "%");
+            }, 500);
+
             defender.health -= attacker.attack;
             attacker.health -= defender.cAttack;
             attacker.attack += attacker.bAttack;
@@ -131,7 +235,7 @@ $(document).ready(function(){
             console.log(defender.name + " now has " + defender.health);
         }
 
-        if (attacker.health == 0){
+        if (attacker.health <= 0){
             alert("You lose!");
         }
         else if (defender.health <= 0){
